ceeb9807a7e48aa1602a5864ca480734c2213039
yaneldodoo <yaneldodoo@gmail.com>
2026-05-19 17:44:49 +0200

    update martca

---
+++ models/marts/mart_ca.sql
@@ -0,0 +1,34 @@
+--this mart is to track the CA 
+--CA mensuel total
+--Nombre de commandes par jour (courbe de tendance)
+--Panier moyen par mois
+--Taux de croissance mois sur mois
+
+WITH base AS (
+
+    SELECT
+        o.order_id,
+        o.ordered_at,
+        DATE_TRUNC('month', o.ordered_at) AS mois_de_commande,
+        DATE_TRUNC('year', o.ordered_at) AS annee_de_commande,
+        oi.total_item_amount
+
+    FROM {{ ref('stg_orders') }} o
+    JOIN {{ ref('stg_order_items') }} oi
+        ON o.order_id = oi.order_id
+
+)
+
+SELECT
+
+    annee_de_commande,
+    mois_de_commande,
+
+    COUNT(DISTINCT order_id) AS nombre_de_commandes,
+    SUM(total_item_amount) AS ca_total,
+    AVG(total_item_amount) AS panier_moyen
+
+FROM base
+
+GROUP BY 1,2
+ORDER BY 1,2
