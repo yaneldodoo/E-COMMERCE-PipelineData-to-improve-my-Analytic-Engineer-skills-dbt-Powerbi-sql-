aad188a2a3c2c7c6593fb43823fd747ed46b2a44
yaneldodoo <yaneldodoo@gmail.com>
2026-05-20 10:30:25 +0200

    update martca

--- models/marts/mart_ca.sql
+++ models/marts/mart_ca.sql
@@ -9,21 +9,21 @@ WITH base AS (
     SELECT
         o.order_id,
         o.ordered_at,
-        DATE_TRUNC('month', o.ordered_at) AS mois_de_commande,
-        DATE_TRUNC('year', o.ordered_at) AS annee_de_commande,
+
+        EXTRACT(YEAR FROM o.ordered_at) AS annee_de_commande,
+        EXTRACT(MONTH FROM o.ordered_at) AS mois_de_commande,
+
         oi.total_item_amount
 
     FROM {{ ref('stg_orders') }} o
+
     JOIN {{ ref('stg_order_items') }} oi
         ON o.order_id = oi.order_id
-
 )
-
 SELECT
 
     annee_de_commande,
     mois_de_commande,
-
     COUNT(DISTINCT order_id) AS nombre_de_commandes,
     SUM(total_item_amount) AS ca_total,
     AVG(total_item_amount) AS panier_moyen
